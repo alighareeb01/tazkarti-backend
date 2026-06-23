74f4882733d79615833ad1e8524c763977556074
alighareeb01 <ghareeb874@gmail.com>
2026-06-23 23:32:28 +0300

    added http://localhost:5173 to cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json({ limit: "10kb" }));
 
 const corsOptions = {
-  origin: [`http://localhost:3000`],
+  origin: [`http://localhost:3000`, "http://localhost:5173'"],
 };
 
 app.use(cors(corsOptions));
