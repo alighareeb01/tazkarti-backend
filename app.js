a551b0f846d9350daea517a6fb0724c6f93ae45c
alighareeb01 <ghareeb874@gmail.com>
2026-06-24 02:54:31 +0300

    fixed cors

--- app.js
+++ app.js
@@ -8,20 +8,12 @@ import cors from "cors";
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
+const corsOptions = {
+  origin: [`http://localhost:3000`, "http://localhost:5173"],
+};
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  }),
-);
+app.use(cors(corsOptions));
+
 app.use(express.json({ limit: "10kb" }));
 
 app.use("/api/users", userRouter);
