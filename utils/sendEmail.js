00dfb4afc0bc17ba34d07162d3927369a7e9419b
alighareeb01 <ghareeb874@gmail.com>
2026-06-24 14:38:21 +0300

    fix email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,24 +5,17 @@ dotenv.config({ path: "./config.env" });
 
 export const sendEmail = async (options) => {
 const transport = nodemailer.createTransport({
-  service: "SendGrid",
+  service: "gmail",
   auth: {
-    user: "apikey",
-    pass: process.env.SENDGRID_PASSWORD,
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_APP_PASSWORD,
   },
   connectionTimeout: 10000, // 10 seconds
   socketTimeout: 10000,
 });
 
-transport.verify((error, success) => {
-  if (error) {
-    console.log("EMAIL ERROR", error);
-  } else {
-    console.log("EMAIL READY TO BE SENT");
-  }
-});
 const mailOptions = {
-  from: `Aly Abdullkareem  <${process.env.MAILTRAP_FROM}>`,
+  from: `Aly Abdullkareem <${process.env.GMAIL_USER}>`,
   to: options.email,
   subject: options.subject,
   html: options.message,
