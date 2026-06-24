51dacc68cb79a3acd210de8c0bd3adb037fce4d5
alighareeb01 <ghareeb874@gmail.com>
2026-06-24 14:51:10 +0300

    test email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,25 +1,51 @@
+// import nodemailer from "nodemailer";
+// import dotenv from "dotenv";
+
+// dotenv.config({ path: "./config.env" });
+
+// export const sendEmail = async (options) => {
+// const transport = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.GMAIL_USER,
+//     pass: process.env.GMAIL_APP_PASSWORD,
+//   },
+//   connectionTimeout: 10000,
+//   socketTimeout: 10000,
+// });
+
+// const mailOptions = {
+//   from: `Aly Abdullkareem <${process.env.GMAIL_USER}>`,
+//   to: options.email,
+//   subject: options.subject,
+//   html: options.message,
+// };
+
+//   return await transport.sendMail(mailOptions);
+// };
+
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "./config.env" });
+dotenv.config();
 
 export const sendEmail = async (options) => {
-const transport = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_APP_PASSWORD,
-  },
-  connectionTimeout: 10000,
-  socketTimeout: 10000,
-});
+  const transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_APP_PASSWORD,
+    },
+  });
 
-const mailOptions = {
-  from: `Aly Abdullkareem <${process.env.GMAIL_USER}>`,
-  to: options.email,
-  subject: options.subject,
-  html: options.message,
-};
+  const info = await transport.sendMail({
+    from: `Aly Abdullkareem <${process.env.GMAIL_USER}>`,
+    to: options.email,
+    subject: options.subject,
+    html: options.message,
+  });
 
-  return await transport.sendMail(mailOptions);
+  console.log("EMAIL INFO:", info);
+
+  return info;
 };
