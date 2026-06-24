378ba41e78895886ef9ce1043a40915e5d217038
alighareeb01 <ghareeb874@gmail.com>
2026-06-24 14:18:42 +0300

    fixe email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -10,8 +10,17 @@ const transport = nodemailer.createTransport({
     user: "apikey",
     pass: process.env.SENDGRID_PASSWORD,
   },
+  connectionTimeout: 10000, // 10 seconds
+  socketTimeout: 10000,
 });
 
+transport.verify((error, success) => {
+  if (error) {
+    console.log("EMAIL ERROR", error);
+  } else {
+    console.log("EMAIL READY TO BE SENT");
+  }
+});
 const mailOptions = {
   from: `Aly Abdullkareem  <${process.env.MAILTRAP_FROM}>`,
   to: options.email,
